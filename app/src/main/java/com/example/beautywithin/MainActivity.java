4c5e944a738d902c16a3409690cfab6a0544f195
Abdirahman Osman <abdirahmanosman@Abdirahmans-MacBook-Pro.local>
2019-05-10 17:23:20 +0300

    added click listener to my makeup button

--- app/src/main/java/com/example/beautywithin/MainActivity.java
+++ app/src/main/java/com/example/beautywithin/MainActivity.java
@@ -4,14 +4,30 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private static  int SPLASH_TIME_OUT = 4000 ;
+    private Button mmakeupButton;
+    private Button mskincareButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mmakeupButton = (Button) findViewById(R.id.makeupButton);
+        mmakeupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MakeupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mskincareButton = (Button) findViewById(R.id.skincareButton);
+
         new Handler() .postDelayed(new Runnable(){
 
             @Override
