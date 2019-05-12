6828938ad795064aba2d0b41bd5ec45088739bf3
Abdirahman Osman <abdirahmanosman@Abdirahmans-MBP.lan>
2019-05-12 21:51:34 +0300

    add member variable to hold my sugn up button

--- app/src/main/java/com/example/beautywithin/signUpActivity.java
+++ app/src/main/java/com/example/beautywithin/signUpActivity.java
@@ -2,8 +2,10 @@ package com.example.beautywithin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class signUpActivity extends AppCompatActivity {
+    private Button msignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
