1f242ea1a4f0233d5ea0624f8e67eb7a0bf7dbce
Abdirahman Osman <abdirahmanosman@Abdirahmans-MBP.lan>
2019-05-12 22:10:46 +0300

    set member variable for  text edit

--- app/src/main/java/com/example/beautywithin/signUpActivity.java
+++ app/src/main/java/com/example/beautywithin/signUpActivity.java
@@ -4,9 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class signUpActivity extends AppCompatActivity {
     private Button msignUpButton;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,7 @@ public class signUpActivity extends AppCompatActivity {
         msignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_LONG).show();
 
             }
         });
