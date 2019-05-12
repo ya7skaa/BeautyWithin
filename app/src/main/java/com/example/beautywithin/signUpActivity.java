5069479f407053db7c6d236ce34318e934a5ac41
Abdirahman Osman <abdirahmanosman@Abdirahmans-MBP.lan>
2019-05-12 21:54:18 +0300

    set my variable within onCreate variable

--- app/src/main/java/com/example/beautywithin/signUpActivity.java
+++ app/src/main/java/com/example/beautywithin/signUpActivity.java
@@ -11,5 +11,7 @@ public class signUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        msignUpButton = (Button) findViewById(R.id.signUpButton);
     }
 }
