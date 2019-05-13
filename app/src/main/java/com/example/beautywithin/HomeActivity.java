70510d5dd4249d416dc6854c1ce1eb0812f82735
Abdirahman Osman <abdirahmanosman@Abdirahmans-MacBook-Pro.local>
2019-05-13 11:02:18 +0300

    added onClick method for makeup button with intent to makeup activity

--- app/src/main/java/com/example/beautywithin/HomeActivity.java
+++ app/src/main/java/com/example/beautywithin/HomeActivity.java
@@ -1,15 +1,28 @@
 package com.example.beautywithin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
-    private Button mMakeupButton;
+    private Button mmakeUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        mmakeUpButton = (Button) findViewById(R.id.makeUpButton);
+
+        mmakeUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, MakeupActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
