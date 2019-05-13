50896ab7c16770de0b05bbc9a1bd0ee64ac0730c
Abdirahman Osman <abdirahmanosman@Abdirahmans-MacBook-Pro.local>
2019-05-13 11:16:13 +0300

    created a string array of makeup products

--- app/src/main/java/com/example/beautywithin/MakeupActivity.java
+++ app/src/main/java/com/example/beautywithin/MakeupActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.widget.ListView;
 
 public class MakeupActivity extends AppCompatActivity {
-    private ListView mListView;
+
+    private String[] makeup = new String[] {"All Day Luminous Foundation","Stunna Lip Paint", "Better Than Sex", "All Nighter Setting Spray","Studio Fix Longwear Foundation","Infallible Proglow Foundation", "Telepathy Super Shock Shadow"};
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
