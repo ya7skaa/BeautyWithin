4a0d44a0a424c20e65704c62719d890e82d7f989
Abdirahman Osman <abdirahmanosman@Abdirahmans-MacBook-Pro.local>
2019-05-13 11:36:29 +0300

    added dependancies that support butterknife binding in build gradle

--- app/src/main/java/com/example/beautywithin/MakeupActivity.java
+++ app/src/main/java/com/example/beautywithin/MakeupActivity.java
@@ -1,13 +1,15 @@
 package com.example.beautywithin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MakeupActivity extends AppCompatActivity {
     private ListView mListView;
 
-    private String[] makeup = new String[] {"All Day Luminous Foundation","Stunna Lip Paint", "Better Than Sex", "All Nighter Setting Spray","Studio Fix Longwear Foundation","Infallible Proglow Foundation", "Telepathy Super Shock Shadow"};
+    private String[] makeups = new String[] {"All Day Luminous Foundation","Stunna Lip Paint", "Better Than Sex", "All Nighter Setting Spray","Studio Fix Longwear Foundation","Infallible Proglow Foundation", "Telepathy Super Shock Shadow"};
 
 
 
@@ -17,5 +19,12 @@ public class MakeupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_makeup);
 
         mListView = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,makeups );
+        mListView.setAdapter(adapter);
+
+
+
+
     }
 }
