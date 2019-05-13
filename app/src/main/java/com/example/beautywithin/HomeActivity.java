9e0755d10c7e5d20d71647130138a6e707e04c65
Abdirahman Osman <abdirahmanosman@Abdirahmans-MacBook-Pro.local>
2019-05-13 10:58:30 +0300

    imported classes for button

--- app/src/main/java/com/example/beautywithin/HomeActivity.java
+++ app/src/main/java/com/example/beautywithin/HomeActivity.java
@@ -2,6 +2,7 @@ package com.example.beautywithin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
     private Button mMakeupButton;
