02f32b84cf5f5ba01c8c52a0cbf657e244c55323
Abdirahman Osman <abdirahmanosman@Abdirahmans-MacBook-Pro.local>
2019-05-13 11:11:55 +0300

    added member variable for list view in makeupActivity.java

--- app/src/main/java/com/example/beautywithin/MakeupActivity.java
+++ app/src/main/java/com/example/beautywithin/MakeupActivity.java
@@ -2,8 +2,10 @@ package com.example.beautywithin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MakeupActivity extends AppCompatActivity {
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
