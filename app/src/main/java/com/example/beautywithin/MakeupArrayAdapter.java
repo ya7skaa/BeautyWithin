bb0b7a60d6e1d88ae546148f8847a26d66689b33
Abdirahman Osman <abdirahmanosman@Abdirahmans-MacBook-Pro.local>
2019-05-13 13:32:08 +0300

    override ArrayAdapter methods and replace with custom ones

--- app/src/main/java/com/example/beautywithin/MakeupArrayAdapter.java
+++ app/src/main/java/com/example/beautywithin/MakeupArrayAdapter.java
@@ -1,5 +1,31 @@
 package com.example.beautywithin;
 
-public class MakeupArrayAdapter ArrayAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class MakeupArrayAdapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mmakeups;
+    private String[] mbrands;
+
+    public MakeupArrayAdapter(Context mContext, int resource, String[] mmakeups, String[] mbrands) {
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mmakeups = mmakeups;
+        this.mbrands = mbrands;
+
+    }
+
+        @Override
+        public Object getItem(int position) {
+            String restaurant = mmakeups[position];
+            String cuisine = mbrands[position];
+            return String.format("%s \nServes great: %s", mmakeups, mbrands);
+        }
+
+        @Override
+        public int getCount() {
+            return mmakeups.length;
+    }
 
 }
